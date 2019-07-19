4411cb5dbfd3b2a058afeb1e671eedef4900fdb5
Johnny <johnny@rushs-MacBook-Pro.local>
2019-07-19 15:07:33 -0600

    added menu and card for caprate calc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule, MatMenuModule, MatInputModule} from '@angular/material';
 import { AppComponent } from './app.component';
+import { ToolbarOverviewComponent } from './toolbar-overview/toolbar.component';
+import { MenuOverviewExample } from './main-menu/main-menu.component';
+import { CardFancyExample } from './calculator-card/card-fancy.component';
+import { CapRateComponent } from './calculator-card/cap-rate-form/cap-rate.component'
+
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarOverviewComponent,
+    MenuOverviewExample,
+    CardFancyExample,
+    CapRateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatMenuModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/toolbar-overview/toolbar.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Basic toolbar
+ */
+@Component({
+  selector: 'toolbar-overview',
+  templateUrl: 'toolbar-overview.html',
+  styleUrls: ['toolbar-overview.css'],
+})
+export class ToolbarOverviewComponent {}
